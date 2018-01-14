731405cfd8fa39d71731d3cccdcf35c93442e7a5
koulao <-123ONdrej>
2018-01-14 21:59:41 +0100

    Edge.equals impemented

--- src/main/java/api/Edge.java
+++ src/main/java/api/Edge.java
@@ -1,6 +1,7 @@
 package api;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 import java.util.Map;
@@ -8,7 +9,11 @@ import java.util.Map;
 @Getter
 @Setter
 public class Edge extends AbstractGraphElement {
+
+    @NonNull
     private Vertex v1;
+
+    @NonNull
     private Vertex v2;
 
     public Edge(Vertex v1, Vertex v2) {
@@ -20,5 +25,18 @@ public class Edge extends AbstractGraphElement {
         super(label, properties);
         this.v1 = v1;
         this.v2 = v2;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (!super.equals(that)) {
+            return false;
+        }
+        if (!(that instanceof Edge)) {
+            return false;
+        }
+        Edge edge = (Edge) that;
+        return this.getV1().equals(edge.getV1())
+                && this.getV2().equals(edge.getV2());
     }
 }
