3a82c17bb988cbcf3e271c723767649bbaefeb6e
Ondrej Koula <koulaond@fel.cvut.cz>
2018-05-25 00:11:51 +0200

    NodeIntrospectorTest.java added relation property holder check + multiValue checking method moved to Utils class

--- src/main/java/repository/schema/introspection/PackageIntrospector.java
+++ src/main/java/repository/schema/introspection/PackageIntrospector.java
@@ -2,14 +2,27 @@ package repository.schema.introspection;
 
 import org.reflections.Reflections;
 import repository.schema.annotations.Node;
+import repository.schema.descriptions.NodeDescription;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
+/**
+ * Simple introspector class that introspects the package, finds all classes with @{@link Node} annotation
+ * and for each class creates @{@link NodeDescription} objects.
+ */
 public class PackageIntrospector {
 
-    public Set<Class<?>> introspectPackage(String pckg){
+    /**
+     * Receives the package name and processes each @{@link Node} annotated class using @{@link NodeIntrospector}.
+     * @param pckg package to ge introspected
+     * @return set of @{@link NodeDescription} instances =
+     */
+    public Set<NodeDescription> introspectPackage(String pckg) {
         Reflections reflections = new Reflections(pckg);
-        Set<Class<?>> nodeTypeAnnotations = reflections.getTypesAnnotatedWith(Node.class);
-        return nodeTypeAnnotations;
+        Set<Class<?>> nodeClasses = reflections.getTypesAnnotatedWith(Node.class);
+        return nodeClasses.stream()
+                .map(nodeClass -> new NodeIntrospector<>(nodeClass).introspect())
+                .collect(Collectors.toSet());
     }
 }
